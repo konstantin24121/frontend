298fdfe41c4f4385a6d912b6fdd903aa76dbcb2e
Vinston Wolf <konstantin24121@gmail.com>
2016-02-08 18:06:38 +0500

    #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,17 @@
 var package = require('./package.json');
-
+var path = require("path");
 
 module.exports = function(grunt) {
 	var mode = grunt.option("mode") || 'dev';
 	
-	global['domain'] = package.domain;
+	//Get path name
+	path = process.cwd().split(path.sep);
+
+	//Set global variables
+	global['domain'] = package.project.domain;
+	global['host']   = package.project.host;
+	global['user']   = package.project.user;
+	global['dest']   = '/www/' + global['domain'] + '/themes/'+ path[path.length - 1];
     
     require('time-grunt')(grunt);
 
--- grunt/ftp_push.js
+++ grunt/ftp_push.js
@@ -1,98 +1,26 @@
-var optionsArray =  {
-        authKey: "mak",
-        host: "88.198.10.230",
-        dest: "/www/arr.dcmr.ru/themes/yohoho/web",
-    };
-module.exports = 
-{
-    css: {
+var optionsArray = {
+    host: global['host'],
+    authKey: global['user'],
+    dest: global['dest']
+};
+
+module.exports = {
+   	all: {
         options: optionsArray,
         files: [{
-            expand: true,
-            cwd: 'web',
             src: [
-                "css/**/*",
-            ]
-        }]
-    },
-    js: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "js/**/*",
-            ]
-        }]
-    },
-    favicons: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "favicon/**/*",
-            ]
-        }]
-    },
-    img: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "img/**/*",
-            ]
-        }]
-    },
-    fonts: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "fonts/**/*",
-            ]
-        }]
-    },
-    svg: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "svg/**/*",
-            ]
-        }]
-    },
-    components: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "components/**/*",
-            ]
-        }]
-    },
-    bower: {
-        options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'web',
-            src: [
-                "bower/**/*",
-            ]
-        }]
-    },
-    views:{
-    	options: optionsArray,
-        files: [{
-            expand: true,
-            cwd: 'view',
-            src: [
-                "/**/*",
-            ]
+                "sources/**/*.*",
+                "views/**/*.*",
+                "web/**/*.*",
+                "grunt/**/*.*",
+                ".bowerrc",
+                ".gitignore",
+                ".htaccess",
+                "*.php",
+                "Gruntfile.js",
+                "package.json",
+                "bower.json",
+            ],
         }]
     }
 }
