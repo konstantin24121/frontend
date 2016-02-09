9ba8aaec07ff25bb193a567c557294c38cd415b6
Vinston Wolf <konstantin24121@gmail.com>
2016-02-09 16:17:12 +0500

    скрипт загрузчика

--- load.js
+++ load.js
@@ -1,21 +1,12 @@
-//TODO get module from global
-
-var Client = require(NODE_PATH + 'ftp'),
-	path = require('path'),
+var	path = require('path'),
 	fs = require('fs');
 
+//Получаем имя пользователя
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+//Подрубаем глобальный пакет
+var Client = require('C:/Users/'+userName+'/AppData/Roaming/npm/node_modules/ftp');
 
-var dest = '/www/five.dcmr.ru/themes/frontend/',
-	root = '.temp/',
-	pathes = [
-		'.bowerrc',
-		'.gitignore',
-		'Gruntfile.js',
-		'bower.json',
-		'package.json',
-		'sources/',
-		'grunt/'
-	];
+
 
 var c = new Client();
 c.on('ready', function() {
@@ -27,22 +18,27 @@ c.on('ready', function() {
             console.log('Error:' + errMsg);
         } 
         c.end();
+        fs.unlink('load.js')
     }
 
+	if (!fs.existsSync(load.root) && load.root !== ''){
+		fs.mkdirSync(load.root);
+	}
+
 	function download() {
-        if (pathes.length < 1) {
+        if (load.pathes.length < 1) {
 
             closeConnection();
             return; // We are completed, close connection and end the program
         }
-        var element = pathes.pop();
+        var element = load.pathes.pop();
         if( /\.+/.test(element) ){
         	//Если файл то будем качать
         	file = element;
-	        c.get(dest + file, function(err, stream) {
+	        c.get(load.dest + file, function(err, stream) {
 				//Получаем в файл контент
 				if (err) throw err;
-				var curDir = root + file;
+				var curDir = load.root + file;
 				stream.pipe(fs.createWriteStream(curDir));
 				console.log('File '+ curDir + ' dowload');
 			 	download();
@@ -51,19 +47,19 @@ c.on('ready', function() {
         	//Если папка то будем делать реккурсивный запрос и вытаскивать все, что только можно
         	dir = element;
         	//И сразу создаем ее локально
-        	if (!fs.existsSync(root + dir)){
-				fs.mkdirSync(root + dir);
+        	if (!fs.existsSync(load.root + dir)){
+				fs.mkdirSync(load.root + dir);
 			}
-        	c.list(dest + dir ,function(err, list) {
+        	c.list(load.dest + dir ,function(err, list) {
 				if (err) throw err;
 
 				//Все файлы и папки в списке добавляем в очередь скачивания
 				list.forEach(function(i, index, arr){
 					if (arr[index].type === 'd'){
 						//Если директория то добавим в конце /
-						pathes.push(dir + arr[index].name + '/');
+						load.pathes.push(dir + arr[index].name + '/');
 					}else{
-						pathes.push(dir + arr[index].name);
+						load.pathes.push(dir + arr[index].name);
 					}
 				})
 				download();
@@ -73,8 +69,26 @@ c.on('ready', function() {
     download();
 });
 
-c.connect({
+//Настройка закачки
+var load = {
 	host: '88.198.10.230',
 	user: 'mak',
-	password: 'gt[frjlth'
+	password: 'gt[frjlth',
+	dest: '/www/five.dcmr.ru/themes/frontend/',
+	root: '',
+	pathes: [
+		'.bowerrc',
+		'.gitignore',
+		'Gruntfile.js',
+		'bower.json',
+		'package.json',
+		'sources/',
+		'grunt/'
+	]
+}
+
+c.connect({
+	host: load.host,
+	user: load.user,
+	password: load.password
 });
